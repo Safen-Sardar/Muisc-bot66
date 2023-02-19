0478d95a78569541e9f492398005d6167f27484a
Safa <72757647+Safen-Sardar@users.noreply.github.com>
2023-02-20 00:04:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-TOKEN: "",
+TOKEN: "OTE3ODk5NzMzNDQ2NTU3Njk3.GYqK-G.Nql3OSRsUhew74mt_nUqH3UIaKaHKgYaifpe-s",
 ownerID: [""], //write your discord user id. example: ["id"] or ["id1","id2"]
 botInvite: "", //write your discord bot invite.
 supportServer: "", //write your discord bot support server invite.
